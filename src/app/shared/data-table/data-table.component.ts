e3d0c5c055a18e385784d55df0071976a4b836ce
gorapiotr <pgora@sigma.ug.edu.pl>
2019-03-29 22:19:01 +0100

    Added pagination to data form

---
+++ src/app/shared/data-table/data-table.component.ts
@@ -0,0 +1,30 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-data-table',
+  templateUrl: './data-table.component.html',
+  styleUrls: ['./data-table.component.scss']
+})
+export class DataTableComponent implements OnInit {
+
+  @Input() data: any;
+  loading = true;
+  page = 1;
+  pageSize = 10;
+  collectionSize: number;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.collectionSize = this.data.length;
+    this.loading = false;
+  }
+
+  getData(): any {
+    return this.data
+      .map((data, i) => ({id: i + 1, ...data}))
+      .slice((this.page - 1) * this.pageSize, (this.page - 1) * this.pageSize + this.pageSize);
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {InputFileComponent} from './forms/input-file/input-file.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { DataChartsComponent } from './data-charts/data-charts.component';
+import {DataChartsComponent} from './data-charts/data-charts.component';
 import {ChartsModule} from 'ng2-charts';
-import { ClassChartComponent } from './data-charts/class-chart/class-chart.component';
-import { PropertiesChartComponent } from './data-charts/properties-chart/properties-chart.component';
+import {ClassChartComponent} from './data-charts/class-chart/class-chart.component';
+import {PropertiesChartComponent} from './data-charts/properties-chart/properties-chart.component';
+import {DataTableComponent} from './data-table/data-table.component';
+import {NgbPaginationModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -13,20 +15,26 @@ import { PropertiesChartComponent } from './data-charts/properties-chart/propert
     //DataCharts
     DataChartsComponent,
     ClassChartComponent,
-    PropertiesChartComponent
+    PropertiesChartComponent,
+
+    DataTableComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
     ChartsModule,
+    NgbPaginationModule
   ],
   exports: [
     InputFileComponent,
     //DataCharts
     DataChartsComponent,
     ClassChartComponent,
-    PropertiesChartComponent
+    PropertiesChartComponent,
+
+    DataTableComponent
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
